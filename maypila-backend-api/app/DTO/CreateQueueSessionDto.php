e173a54e42d6cbb740c3b23c74a33d5afa8a5f16
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-05-01 18:18:16 +0800

    DTO updates

---
+++ maypila-backend-api/app/DTO/CreateQueueSessionDto.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\DTO;
+
+class CreateQueueSessionDto
+{
+    public function __construct(
+        public readonly string $name,
+        public readonly string $email,
+        public readonly string $password,
+    ) {}
+}
