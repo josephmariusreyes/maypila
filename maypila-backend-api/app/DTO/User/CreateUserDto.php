f0b94f4d2139672af49d22a5b2eeb6dbee6a6ab0
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-05-06 09:14:05 +0800

    app DTO updates

--- maypila-backend-api/app/DTO/User/CreateUserDto.php
+++ maypila-backend-api/app/DTO/User/CreateUserDto.php
@@ -4,8 +4,11 @@ namespace App\DTO\User;
 class CreateUserDto
 {
     public function __construct(
-        public readonly string $name,
-        public readonly string $email,
-        public readonly string $password,
+        public readonly ?string $name,
+        public readonly ?string $email,
+        public readonly ?string $password,
+        public readonly ?string $role,
+        public readonly int $companyId,
+        public readonly int $mobile_number
     ) {}
 }
--- maypila-backend-api/app/DTO/User/UpdateUserDto.php
+++ maypila-backend-api/app/DTO/User/UpdateUserDto.php
@@ -5,9 +5,11 @@ class UpdateUserDto
 {
     public function __construct(
         public readonly int $id,
-        public readonly ?string $name = null,
-        public readonly ?string $email = null,
-        public readonly ?string $password = null,
+        public readonly ?string $name,
+        public readonly ?string $email,
+        public readonly ?string $password,
+        public readonly ?string $role,
+        public readonly int $mobile_number
     ) {}
 
 
