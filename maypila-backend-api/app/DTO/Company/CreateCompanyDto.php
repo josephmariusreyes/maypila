c33f78086355121b44dcf68a6f013332425560a6
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-05-23 16:01:34 +0800

    5/23 - Added company_email in request

--- maypila-backend-api/app/DTO/Company/CreateCompanyDto.php
+++ maypila-backend-api/app/DTO/Company/CreateCompanyDto.php
@@ -5,6 +5,7 @@ class CreateCompanyDto
 {
     public function __construct(
         public readonly string $name,
+        public readonly string $company_email,
         public readonly string $description
     )
     {
