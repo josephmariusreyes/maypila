b3296cff40c7f40f5141c6be193a607ce8d19a12
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-05-23 16:02:00 +0800

    5/22 -  getAppMenu implementation

--- maypila-backend-api/app/Http/Controllers/AccessControlController.php
+++ maypila-backend-api/app/Http/Controllers/AccessControlController.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ApiBaseResponse;
+use App\Services\UserService\UserService;
 use Illuminate\Http\Request;
 
 class AccessControlController extends Controller
 {
-    public function getAppMenu() {
-
+    public function __construct(private UserService $userService)
+    {
     }
 
-    public function createCompanyAndUser() {
-        
+    public function getAppMenu(Request $request)
+    {
+        $menu = $this->userService->getAppMenu($request->user());
+
+        return ApiBaseResponse::success(
+            data: $menu,
+            message: 'App menu fetched successfully'
+        );
+    }
+
+    public function createCompanyAndUser()
+    {
     }
 }
