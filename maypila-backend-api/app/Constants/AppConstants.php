cff1154a886ae4f5f8ee8b6ddedcdc07ed899b0a
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-05-23 16:01:12 +0800

    5/23 - Introduced app constants

---
+++ maypila-backend-api/app/Constants/AppConstants.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Constants;
+
+class AppConstants {
+
+    public const Menu = [
+        [
+            'key' => 'startQueueSession',
+            'text' => 'Start queue session'
+        ],
+        [
+            'key' => 'createNewCompany',
+            'text' => 'Create new company'
+        ],
+        [
+            'key' => 'manageUsers',
+            'text' => 'Manage users'
+        ],
+        [
+            'key' => 'dashboard',
+            'text' => 'Dashboard'
+            //list of queue session can be seen here
+            //users
+            //analytics
+        ],
+        [
+            'key' => 'addUsers',
+            'text' => 'Create queue admin'
+        ]
+    ];
+}
