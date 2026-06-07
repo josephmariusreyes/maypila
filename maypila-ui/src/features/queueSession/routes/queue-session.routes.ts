b327f0af126c4b0a9d13b2f2253f7c2afabe3c9d
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-06-08 06:37:48 +0800

    queue session routes

---
+++ maypila-ui/src/features/queueSession/routes/queue-session.routes.ts
@@ -0,0 +1,84 @@
+import type { RouteRecordRaw } from 'vue-router'
+import appMainLayout from '@/components/layouts/appMainLayout.vue';
+import { UserRole } from '@/features/company/enums/userRoleEnums';
+import QueueSessionListingPage from '@/features/queueSession/pages/queueSessionListingPage.vue';
+import AddCustomerToQueuePage from '@/features/queueSession/pages/addCustomerToQueuePage.vue';
+import CreateQueueSessionPage from '@/features/queueSession/pages/createQueueSessionPage.vue';
+import QueueDetailsPage from '@/features/queueSession/pages/queueDetailsPage.vue';
+import CustomerStatusPage from '@/features/queueSession/pages/customerStatusPage.vue';
+import PublicLayout from '@/components/layouts/publicLayout.vue';
+
+export const queueSessionRoutes: RouteRecordRaw = {
+    path:'/queue-session',
+    component:appMainLayout,
+    meta:{
+		requiresAuth: true,
+    },
+    children:[
+        //Public routes
+        {
+            path:'',
+            redirect:{
+                name:'customer-status'
+            }
+        },
+        {
+            path:'',
+            component: PublicLayout,
+            meta:{
+                guestOnly:true
+            },
+            children:[
+                {
+                    path:'customer-status',
+                    name:'customer-status',
+                    component:CustomerStatusPage
+                }
+            ]
+        },
+        //logged in routes
+        {
+            path:'',
+            component:appMainLayout,
+            meta:{
+                requiresAuth:true
+            },    
+            children:[
+                {
+                    path:'queue-listing',
+                    name:'queue-listing',
+                    meta: {
+                        requiredRoles: [UserRole.CompanyAdmin],
+                    },
+                    component:QueueSessionListingPage
+                },
+                {
+                    path:'add-customer-to-que',
+                    name:'add-customer-to-que',
+                    meta: {
+                        requiredRoles: [UserRole.QueEncoder],
+                    },
+                    component:AddCustomerToQueuePage
+                },
+                {
+                    path:'queue-details',
+                    name:'queue-details',
+                    meta: {
+                        requiredRoles: [UserRole.QueAdmin],
+                    },
+                    component:QueueDetailsPage
+                },
+                {
+                    path:'create-queue-session',
+                    name:'create-queue-session',
+                    meta: {
+                        requiredRoles: [UserRole.CompanyAdmin],
+                    },
+                    component:CreateQueueSessionPage
+                }
+            ]
+        }
+        
+    ]
+
+}
