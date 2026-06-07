7e558555319dc270c4c2402a3c38375c736e60c7
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-06-08 06:38:35 +0800

    main.ts and tsconfig.app.json updates

--- maypila-ui/src/main.ts
+++ maypila-ui/src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from '@/app/router'
 
 import { registerAppProviders } from '@/app/providers/app-provider'
 
 const app = createApp(App)
 
 registerAppProviders(app)
+app.use(router)
 
 app.mount('#app')
