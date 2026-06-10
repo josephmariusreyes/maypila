14e3f5800e56a62b478ad3e751f9343696981e26
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-06-10 11:43:11 +0800

    add global styling and global theme

--- maypila-ui/src/main.ts
+++ maypila-ui/src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import router from '@/app/router'
+
+import './style.css'
 
 import { registerAppProviders } from '@/app/providers/app-provider'
 
