653ec564bcd2880809368b9eb06da3ff407098eb
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-06-17 08:17:15 +0800

    Create company page

--- maypila-ui/src/features/company/pages/createCompanyPage.vue
+++ maypila-ui/src/features/company/pages/createCompanyPage.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+import Card from '@/components/ui/card/Card.vue';
+import CreateCompanyForm from '../component/CreateCompanyForm.vue';
+</script>
+
 <template>
-    create company page
+    <section class="flex
+	w-full
+	max-w-md
+	justify-center">
+        <div class="min-w-120 max-w-130">
+            <Card class="p-6 mb-6">
+                <strong class="text-left text-1xl">
+                </strong>
+                <strong class="text-left font-main-theme-color text-2xl">
+                    Create Account.
+                </strong>
+                <p class="text-sm leading-6 text-slate-600">
+                    After submitting sign up form <strong>AddToQueue</strong> will send an email for your login credentials.
+                </p>
+            </Card>
+            <CreateCompanyForm />
+        </div>
+    </section>
 </template>
