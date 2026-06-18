ea24379b994068494839f68e2097e52e00c497ae
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-06-18 11:22:40 +0800

    AuthController and base resource updates

--- maypila-backend-api/app/Http/Controllers/AuthController.php
+++ maypila-backend-api/app/Http/Controllers/AuthController.php
@@ -9,57 +9,57 @@ use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use App\Http\Requests\Auth\LoginAuthRequest;
 use App\Http\Resources\Customer\CustomerResource;
+use App\Http\Resources\User\UserResource;
+use App\Http\Resources\ApiBaseResponseResource;
+use App\Http\Resources\UserResponseResource;
+use Illuminate\Auth\Access\AuthorizationException;
+
+use Knuckles\Scribe\Attributes\ResponseFromApiResource;
+
 
 class AuthController extends Controller
 {
-	/**
-	 * Login user
-	 *
-	 * @response 200 {
-	 *   "message": "Login successful.",
-	 *   "token": "1|abcdef1234567890",
-	 *   "user": {
-	 *     "id": 1,
-	 *     "name": "John Doe",
-	 *     "email": "john@example.com",
-	 *     "created_at": "2026-06-16T10:00:00.000000Z",
-	 *     "updated_at": "2026-06-16T10:00:00.000000Z"
-	 *   }
-	 * }
-	 */	
-	public function login(LoginAuthRequest $request): JsonResponse
+	#[ResponseFromApiResource(
+		ApiBaseResponseResource::class,
+		User::class
+	)]
+	public function login(LoginAuthRequest $request): ApiBaseResponseResource
 	{
-		//todojeph: move this logic to service
-		
 		$credentials = $request->validated();
 		$user = User::where('email', $credentials['email'])->first();
 		$companies = $user->companies;
 		$roles = $user->roles;
 
 		if (!$user || !Hash::check($credentials['password'], $user->password)) {
-			return response()->json([
-				'message' => 'The provided credentials are incorrect.',
-			], 401);
+			throw new AuthorizationException('The provided credentials are incorrect.');
 		}
 
 		$tokenName = $credentials['device_name'] ?? 'api-token';
 		$token = $user->createToken($tokenName)->plainTextToken;
 
-		return response()->json([
-			'message' => 'Login successful.',
-			'token' => $token,
-			'user' =>new CustomerResource(
-               $user
-            )
-		]);
+
+		return new ApiBaseResponseResource(
+			resource: UserResource::make($user),
+			success: true,
+			message:'Login successful.',
+			meta: [
+				'token_name' => $tokenName,
+				'token' => $token
+			]
+		);
 	}
 
-	public function logout(Request $request): JsonResponse
+	#[ResponseFromApiResource(
+		ApiBaseResponseResource::class
+	)]
+	public function logout(Request $request): ApiBaseResponseResource
 	{
 		$request->user()->currentAccessToken()?->delete();
 
-		return response()->json([
-			'message' => 'Logout successful.',
-		]);
+		return new ApiBaseResponseResource(
+			resource: [],
+			success: true,
+			message: 'Logout successful.'
+		);
 	}
 }
