d30bca637fbc71ebd02c3d080660745300aa505f
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-06-28 08:39:15 +0800

    Dashboard updates

--- maypila-ui/src/features/dashboard/routes/dashboard.routes.ts
+++ maypila-ui/src/features/dashboard/routes/dashboard.routes.ts
@@ -1,6 +1,7 @@
 import type { RouteRecordRaw } from 'vue-router'
 import appMainLayout from '@/components/layouts/AppMainLayout.vue'
 import DashboardPage from '@/features/dashboard/pages/dashboardPage.vue'
+import { UserRole } from '@/features/company/enums/userRoleEnums'
 
 export const dashboardRoutes: RouteRecordRaw = {
 	path: '/dashboard',
@@ -12,6 +13,9 @@ export const dashboardRoutes: RouteRecordRaw = {
 		{
 			path: '',
 			name: 'dashboard',
+			meta: {
+				requiredRoles: [UserRole.SuperAdmin],
+			},
 			component: DashboardPage,
 		},
 	],
